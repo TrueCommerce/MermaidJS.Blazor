9fe1c345e90db47e6bebb502640824cbce9a3a88
Josh Johnson <DotJoshJohnson@users.noreply.github.com>
2021-06-25 15:31:38 -0400

    Release 1.0.0 (#1)

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # MermaidJS.Blazor
+
 A simple MermaidDiagram component for Blazor.
+
+## Getting Started
+
+* Install the `MermaidJS.Blazor` NuGet package.
+
+  ```bash
+  > dotnet add package MermaidJS.Blazor
+  ```
+
+* Add `builder.Services.AddMermaidJS()` to your `Program.cs`.
+
+  ```csharp
+  using Microsoft.Extensions.DependencyInjection;
+
+  // ... //
+
+  builder.Services.AddMermaidJS();
+  ```
+
+* Use the `MermaidDiagram` component in your app.
+
+  ```html
+  @using MermaidJS.Blazor
+
+  <!-- ... -->
+
+  <MermaidDiagram Definition="@diagramDefinition" OnClick="OnClickNode" />
+
+  @code
+  {
+      string diagramDefinition = "graph TB\nA-->B";
+
+      void OnClickNode(string nodeId)
+      {
+          // TODO: do something with nodeId
+      }
+  }
+  ```
+
+## Handling Node Clicks
+
+To raise node click events back to your blazor component, set your click callback to `onClickMermaidNode`:
+
+```mermaid
+graph TB
+A--B
+click A onClickMermaidNode
+```
---
+++ src/MermaidJS.Blazor/wwwroot/MermaidDiagramInterop.js
@@ -0,0 +1,81 @@
+﻿function initializeGlobals() {
+    function loadMermaid() {
+        return new Promise((resolve) => {
+            if (!window.mermaid) {
+                const script = document.createElement("script");
+
+                script.src = "_content/MermaidJS.Blazor/lib/mermaid/mermaid.min.js";
+                script.async = false;
+                script.defer = false;
+                script.onload = () => {
+                    window.mermaid.mermaidAPI.initialize({ securityLevel: "loose" });
+
+                    resolve();
+                };
+
+                document.body.appendChild(script);
+            }
+
+            else {
+                resolve();
+            }
+        });
+    }
+
+    return loadMermaid().then(() => {
+        if (!window.mermaidDiagramBlazorComponents) {
+            window.mermaidDiagramBlazorComponents = new Map();
+        }
+
+        if (!window.onClickMermaidNode) {
+            window.onClickMermaidNode = function (nodeId) {
+                window.mermaidDiagramBlazorComponents.forEach((componentRef, componentId) => {
+                    try {
+                        componentRef.invokeMethodAsync("OnClickMermaidNode", nodeId);
+                    }
+
+                    catch (err) {
+                        console.error(`Failed to invoke Mermaid node click handler for component: ${componentId}.`, err);
+                    }
+                });
+            }
+        }
+    });
+}
+
+export function beginRender(componentId, definition) {
+    initializeGlobals().then(() => {
+        const componentRef = window.mermaidDiagramBlazorComponents.get(componentId);
+
+        if (!componentRef) {
+            return;
+        }
+
+        try {
+            window.mermaid.mermaidAPI.render(`${componentId}-svg`, definition, (svg, bind) => {
+                const host = document.getElementById(componentId);
+
+                host.innerHTML = svg;
+
+                bind(host);
+
+                componentRef.invokeMethodAsync("OnRenderCompleted");
+            });
+        }
+
+        catch (err) {
+            componentRef.invokeMethodAsync("OnRenderFailed", err.message);
+        }
+    });
+}
+
+export function registerComponent(componentId, componentRef) {
+    initializeGlobals().then(() => {
+        window.mermaidDiagramBlazorComponents.set(componentId, componentRef);
+    });
+}
+
+export function unregisterComponent(componentId) {
+    window.mermaidDiagramBlazorComponents.delete(componentId);
+}
+
